f93bba0bf5ababa127ebde8c0e92a52aac4e1578
Wendy Johana Escobar Espinosa <wescobar597@unab.edu.co>
2025-08-20 10:05:46 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,82 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+
+// Punto 1
+
+public class Main {
+    public static void main(String[] args) {
+        int Numero = 8;
+
+        if (Numero >= 0) {
+            System.out.println("Positivo");
+        } else if (Numero < 0) {
+            System.out.println("Negativo");
+        }
+    }
+}
+
+
+// Punto 2
+
+public class Main {
+    public static void main(String[] args) {
+        int Edad = 8;
+
+        if (Edad >= 18) {
+            System.out.println("Eres mayor de edad");
+        } else if (Edad < 18) {
+            System.out.println("Eres menor de edad");
+        }
+    }
+}
+
+
+// Punto 3
+
+public class Main {
+    public static void main(String[]args) {
+        int Numero1 = 8;
+        int Numero2 = 1;
+
+        if (Numero1 >= Numero2) {
+            System.out.println("El mayor es " + numero1);
+        } else {
+            System.out.println("El mayor es " + numero2);
+        }
+    }
+}
+
+
+// Punto 4
+
+public class Main {
+    public static void main(String[] args) {
+        int calificacion = 40;
+
+        if (calificacion >= 60) {
+            System.out.println("Aprobaste");
+        } else {
+            System.out.println("Reprobaste");
+        }
+    }
+}
+
+
+// Punto 5
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 16;
+
+        if (numero % 2 == 0) {
+            System.out.println(numero + "Es par");
+        } else {
+            System.out.println(numero + "Es impar");
+        }
+    }
+}
+
